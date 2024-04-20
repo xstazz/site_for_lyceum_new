38e949c765151749721821b7c3d910cbfda1df76
xstazz <120487809+xstazz@users.noreply.github.com>
2024-04-20 12:17:00 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -22,8 +22,6 @@ chef_password = 'qwerty'
 
 conn = sqlite3.connect('users.db')
 cursor = conn.cursor()
-
-# Создаем таблицу пользователей (если она не существует)
 cursor.execute('''CREATE TABLE IF NOT EXISTS users (
                   id INTEGER PRIMARY KEY AUTOINCREMENT,
                   username TEXT UNIQUE NOT NULL,
@@ -46,7 +44,6 @@ def upload_avatar():
         if 'avatar' in request.files:
             avatar = request.files['avatar']
             if avatar.filename != '':
-                # Сохраняем аватарку в папку 'avatars' внутри папки 'static'
                 avatar.save(os.path.join(app.root_path, 'static', 'avatars', username + '.jpg'))
                 flash('Аватарка успешно загружена.', 'success')
             else:
@@ -62,23 +59,14 @@ def upload_avatar():
 @app.route('/send_to_active_orders', methods=['POST'])
 def send_to_active_orders():
     global users_order
-
     conn = sqlite3.connect('orders.db')
     cursor = conn.cursor()
-
-    # Получаем заказы из базы данных
     cursor.execute("SELECT dish_name, dish_price FROM orders")
     orders = cursor.fetchall()
-
-    # Добавляем заказы в словарь users_order
     for order in orders:
         users_order[order[0]] = order[1]
-
     conn.close()
-
     flash("Заказы успешно переданы в активные заказы!", 'success')
-
-    # Возвращаемся на админскую панель после отправки заказов
     return redirect(url_for('admin_panel'))
 
 
@@ -87,21 +75,16 @@ def login_post():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
         if username == admin_username and password == admin_password:
             session['username'] = username
             flash(f"Добро пожаловать, {username}!", 'success')
-            return redirect(url_for('admin_panel'))  # Перенаправляем на админ панель
-
+            return redirect(url_for('admin_panel'))
         elif username == chef_username and password == chef_password:
             session['username'] = username
             flash(f"Добро пожаловать, {username}!", 'success')
-            return redirect(url_for('chief'))  # Перенаправляем на страницу меню для повара
-
+            return redirect(url_for('chief'))
         conn = sqlite3.connect('users.db')
         cursor = conn.cursor()
-
-        # Поиск пользователя в базе данных
         cursor.execute("SELECT username, password FROM users WHERE username = ?", (username,))
         user = cursor.fetchone()
 
@@ -124,14 +107,11 @@ def register():
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
-
         if password != confirm_password:
             flash("Пароли не совпадают. Пожалуйста, попробуйте снова.", 'error')
         else:
             conn = sqlite3.connect('users.db')
             cursor = conn.cursor()
-
-            # Вставляем данные пользователя в базу данных
             cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
                            (username, email, password))
 
@@ -153,7 +133,7 @@ def admin_panel():
         cursor.execute("SELECT * FROM orders")
         orders = cursor.fetchall()
 
-        total_amount = sum(order[3] for order in orders)  # Вычисляем итоговую сумму заказов
+        total_amount = sum(order[3] for order in orders)
 
         conn.close()
         return render_template('admin_panel.html', data=orders, total_amount=total_amount)
@@ -165,13 +145,8 @@ def admin_panel():
 @app.route('/active_orders')
 def active_orders():
     global users_order
-
-    # Получаем заказы из словаря для отображения
     orders = users_order.items()
-
-    # Очищаем словарь после отображения данных
     users_order = {}
-
     return render_template('active_order.html', orders=orders)
 
 
@@ -180,12 +155,9 @@ def clear_orders():
     if 'username' in session and session['username'] == admin_username:
         conn = sqlite3.connect('orders.db')
         cursor = conn.cursor()
-
-        # Clear the orders table
         cursor.execute("DELETE FROM orders")
         conn.commit()
         conn.close()
-
         flash("Заказы успешно очищены.", 'success')
         return redirect(url_for('admin_panel'))
     else:
@@ -195,9 +167,7 @@ def clear_orders():
 
 @app.route('/complete_orders', methods=['POST'])
 def complete_orders():
-    global users_order  # Объявляем глобальную переменную
-
-    # Добавляем данные из order в словарь перед удалением
+    global users_order
     for dish in order:
         users_order[dish['id']] = {'name': dish['name'], 'price': dish['price']}
     order.clear()
@@ -229,8 +199,7 @@ def view_order():
 
 @app.route('/pay_order')
 def pay_order():
-    global users_order  # Объявляем глобальную переменную
-
+    global users_order
     conn = sqlite3.connect('orders.db')
     cursor = conn.cursor()
     for dish in order:
@@ -238,7 +207,6 @@ def pay_order():
                 INSERT INTO orders (dish_id, dish_name, dish_price)
                 VALUES (?, ?, ?)
             ''', (dish['id'], dish['name'], dish['price']))
-        # Добавляем данные в словарь перед удалением из order
         users_order[dish['id']] = {'name': dish['name'], 'price': dish['price']}
     conn.commit()
     conn.close()
@@ -248,9 +216,7 @@ def pay_order():
 
 @app.route('/clear_order')
 def clear_order():
-    global users_order  # Объявляем глобальную переменную
-
-    # Добавляем данные из order в словарь перед удалением
+    global users_order
     for dish in order:
         users_order[dish['id']] = {'name': dish['name'], 'price': dish['price']}
     order.clear()
@@ -270,14 +236,11 @@ def chief():
 def profile():
     if 'username' in session:
         username = session['username']
-
         conn = sqlite3.connect('users.db')
         cursor = conn.cursor()
-
         cursor.execute("SELECT username, email FROM users WHERE username = ?", (username,))
         user = cursor.fetchone()
         conn.close()
-
         if user:
             username = user[0]
             email = user[1]
@@ -295,13 +258,11 @@ def update_dish():
     dish_id = int(request.form['dish_id'])
     new_name = request.form['new_name']
     new_price = float(request.form['new_price'])
-
     for dish in menu_list:
         if dish['id'] == dish_id:
             dish['name'] = new_name
             dish['price'] = new_price
             break
-
     flash("Товар успешно обновлен.", 'success')
     return redirect(url_for('chief'))
 
@@ -312,14 +273,9 @@ def add_new_menu():
         if request.method == 'POST':
             new_name = request.form['new_name']
             new_price = float(request.form['new_price'])
-
-            # Generate a unique ID for the new dish
             new_id = max(dish['id'] for dish in menu_list) + 1 if menu_list else 1
-
-            # Create a dictionary for the new dish and add it to the menu_list
             new_dish = {"id": new_id, "name": new_name, "price": new_price}
             menu_list.append(new_dish)
-
             flash("Новое блюдо успешно добавлено.", 'success')
             return redirect(url_for('chief'))
         else:
